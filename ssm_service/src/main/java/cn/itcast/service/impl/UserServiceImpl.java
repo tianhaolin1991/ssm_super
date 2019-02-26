ccb421be744a43c64ab0bc9cb33b1adc1416ae7a
tianhaolin1991 <tianhaolin1991@126.com>
2019-02-26 16:07:22 +0800

    version 1.4.1 spring-security-by-passwordEncoder

--- ssm_service/src/main/java/cn/itcast/service/impl/UserServiceImpl.java
+++ ssm_service/src/main/java/cn/itcast/service/impl/UserServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -18,11 +19,13 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserDao userDao;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserInfo userInfo = userDao.findByName(username);
-        User user = new User(userInfo.getUsername(), "{noop}"+userInfo.getPassword(),
+        User user = new User(userInfo.getUsername(), passwordEncoder.encode(userInfo.getPassword()),
                 userInfo.getStatus() == 1?true:false, true, true, true,
                 getAuthorities(userInfo.getRoles()));
         return user;
